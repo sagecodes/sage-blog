efd0516213d9e800e1cc248489593f02e9530af6
Sage Elliott <wildtroll@gmail.com>
2016-07-05 20:37:30 -0700

    Feat: Create mobile sliding style menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+   $(".sliding-panel-content").on("click", "li", function () {
+       $(".sliding-panel-button").click();
+ });
+});
